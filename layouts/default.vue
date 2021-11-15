d66a4303592777dea2de1cedde6bde6b4061c8fb
michalg070 <diegonbth@gmail.com>
2021-11-15 21:19:47 +0100

    chore 🔥: create layouts, add scss support, remove default components

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="default">
+    <Nuxt />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import url('@/assets/scss/layouts/default.scss');
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="error">
+    <p class="error">error</p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'error',
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('@/assets/scss/layouts/error.scss');
+</style>
