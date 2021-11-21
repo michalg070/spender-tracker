524dbcc008fa39b6cc1264dbb98b7c952668dd4a
michalg070 <diegonbth@gmail.com>
2021-11-21 21:38:36 +0100

    fix 🐛: remove unused imports

--- pages/login.vue
+++ pages/login.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-import { defineComponent, ref, watch } from '@nuxtjs/composition-api'
+import { defineComponent } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   layout: 'login',
