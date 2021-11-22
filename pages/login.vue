f37c06ae271669af51aa09828befb05acb246036
michalg070 <diegonbth@gmail.com>
2021-11-22 21:26:12 +0100

    chore 🔥: add svg component support

--- pages/login.vue
+++ pages/login.vue
@@ -2,15 +2,21 @@
   <div class="login">
     <BaseCard title="Zaloguj się" class="login__card">
       <BaseInput name="login" label="Login" />
+
+      <EyeIcon width="12px" />
     </BaseCard>
   </div>
 </template>
 
 <script>
 import { defineComponent } from '@nuxtjs/composition-api'
+import EyeIcon from '@/assets/images/svg/eye.svg'
 
 export default defineComponent({
   layout: 'login',
+  components: {
+    EyeIcon,
+  },
   setup() {},
 })
 </script>
